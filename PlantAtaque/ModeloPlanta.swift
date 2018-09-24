524249ba16f4fc4ab1d6fb1a3075b1fc6d1cfcb5
VictorLealdeAlmeida <vlpaa@cin.ufpe.br>
2018-09-24 14:58:34 -0300

    PlantAtaque 1.0

---
+++ PlantAtaque/ModeloPlanta.swift
@@ -0,0 +1,9 @@
+//
+//  ModeloPlanta.swift
+//  PlantAtaque
+//
+//  Created by Victor Leal Porto de Almeida Arruda on 24/09/2018.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import Foundation
